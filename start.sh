7935b470a15f0fc63c082edf47896cd9df2195a3
Imanshadowww <imanshadowww@atomicmail.io>
2026-09-10 03:15:35 +0330

    Update

--- start.sh
+++ start.sh
@@ -5,9 +5,9 @@ tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --state=mem
 
 sleep 3
 
-# اتصال به اکانت تو با استفاده از کلید
-tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=render-gamer --accept-routes --advertise-exit-node
+# اتصال به اکانت تو با اصلاح مشکل DNS و اضافه کردن ephemeral
+tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=my-custom-node --accept-routes --advertise-exit-node --accept-dns=false --ephemeral
 
-# اجرای یک وب‌سایت فیک روی پورتی که رندر می‌خواد تا سرور رو خاموش نکنه
+# اجرای یک وب‌سایت فیک روی پورتی که سایت میزبان میخواد تا سرور رو خاموش نکنه
 echo "Tailscale is running! Starting dummy web server..."
-python3 -m http.server $PORT
+python3 -m http.server ${PORT:-8080}
