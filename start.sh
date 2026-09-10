19c3f46ec857c7d2fdeaede96b23cfd96e72d475
Imanshadowww <imanshadowww@atomicmail.io>
2026-09-10 03:47:43 +0330

    Refactor start.sh
    Removed the sleep command and adjusted the tailscale up command.

--- start.sh
+++ start.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-# اول از همه وب‌سرور را روی پورت 8000 در پس‌زمینه روشن می‌کنیم تا پنل ارور ندهد
 echo "Starting dummy web server on port 8000..."
 python3 -m http.server 8000 &
 
-# حالا هسته‌ی تیل‌اسکیل را اجرا می‌کنیم
 tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --state=mem: &
 sleep 3
 
-# و در نهایت به اکانت متصل می‌شویم
-tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=Apply-Node --advertise-exit-node --accept-dns=false --ephemeral
+tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=Apply-Node --advertise-exit-node --accept-dns=false
 
-# این دستور باعث می‌شود کانتینر بیدار بماند و خاموش نشود
 wait
